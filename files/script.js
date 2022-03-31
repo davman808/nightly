891d444e08b1a4f22e2c27a3ff88d7452d4ada29
davman808 <da.seyder@gmail.com>
2022-03-31 09:11:48 +0200

    script now loads last, wallpapers load at random

--- files/script.js
+++ files/script.js
@@ -1,13 +1,11 @@
 setInterval(displayClock, 100);
-
+let wallpaperNumber = Math.round(Math.random() * 17).toLocaleString("en-US", {minimumIntegerDigits: 2, useGrouping: false});
+document.body.style.backgroundImage ="url(images/" + wallpaperNumber + ".jpg)";
 function displayClock() {
     const time = document.querySelector('#time');
     let date = new Date();
     let hrs = date.getHours();
     let min = date.getMinutes();
-    if (hrs > 12) {
-        hrs = hrs - 12
-    }
     if (hrs < 10) {
         hrs = "0" + hrs
     }
